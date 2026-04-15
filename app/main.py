3e05affe6f1c8f3a701fd36696bd114d9258a324
AdekWhat1 <mr.denisyakubenko@gmail.com>
2026-04-15 21:16:53 +0200

    refactor: removed extra spaces and unused variables

--- app/main.py
+++ app/main.py
@@ -23,19 +23,19 @@ def main() -> QuerySet:
     for genre in genres:
         Genre.objects.create(name=genre)
 
-    update_genre = Genre.objects.filter(
+    Genre.objects.filter(
         name="Dramma"
     ).update(name="Drama")
 
-    update_actor_klooney = Actor.objects.filter(
+    Actor.objects.filter(
         last_name="Klooney"
     ).update(last_name="Clooney")
 
-    update_actor_kianu = Actor.objects.filter(
+    Actor.objects.filter(
         first_name="Kianu", last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
 
-    delete_genre = Genre.objects.filter(name="Action").delete()
-    delete_actresses = Actor.objects.filter(first_name="Scarlett").delete()
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
