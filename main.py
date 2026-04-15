271e82d9e9d33c46ccaae2919e5ad64baf3728f1
AdekWhat1 <mr.denisyakubenko@gmail.com>
2026-04-15 21:05:11 +0200

    refactor: rewrite create logic using loops to follow DRY

--- main.py
+++ main.py
@@ -6,21 +6,31 @@ from db.models import Actor
 
 
 def main() -> QuerySet:
-    genre_creat = [
-    Genre.objects.create(name="Western"),
-    Genre.objects.create(name="Action"),
-    Genre.objects.create(name="Dramma")
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johanson"),
     ]
-    actor_creat = [
-        Actor.objects.create(first_name="George", last_name="Klooney"),
-        Actor.objects.create(first_name="Kianu", last_name="Reaves"),
-        Actor.objects.create(first_name="Scarlett", last_name="Keegan"),
-        Actor.objects.create(first_name="Will", last_name="Smith"),
-        Actor.objects.create(first_name="Jaden", last_name="Smith"),
-        Actor.objects.create(first_name="Scarlett", last_name="Johansson"),
-    ]
-    update_genre = Genre.objects.filter(name="Dramma").update(name="Drama")
-    update_actor_klooney = Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+
+    for first_name, last_name in actors:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
+
+    genres = ["Western", "Action", "Drama"]
+
+    for genre in genres:
+        Genre.objects.create(name=genre)
+
+    update_genre = Genre.objects.filter(
+        name="Dramma"
+    ).update(name="Drama")
+
+    update_actor_klooney = Actor.objects.filter(
+        last_name="Klooney"
+    ).update(last_name="Clooney")
+
     update_actor_kianu = Actor.objects.filter(
         first_name="Kianu", last_name="Reaves"
     ).update(first_name="Keanu", last_name="Reeves")
